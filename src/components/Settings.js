aaafc927bbd5a3bf311af69bec0b5145d364b52c
Grzegorz <g.juzba@gmail.com>
2022-03-31 19:49:40 +0200

    Set and Save time in local storage

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -4,14 +4,65 @@ import { faGear, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Settings.css";
 
 class Settings extends Component {
-  state = { settingsIsActive: false };
+  state = {
+    settingsIsActive: false,
+    sessionTime: 25,
+    breakTime: 5,
+  };
 
+  componentDidMount() {
+    const sessionTimeLocal = localStorage.getItem("sessionTime");
+    const breakTimeLocal = localStorage.getItem("breakTime");
+    if (sessionTimeLocal && breakTimeLocal) {
+      this.setState({
+        sessionTime: sessionTimeLocal,
+        breakTime: breakTimeLocal,
+      });
+    }
+  }
   handleShowHideSettings = () => {
     this.setState((prevState) => ({
       settingsIsActive: !prevState.settingsIsActive,
     }));
   };
 
+  timeSubtraction = (e) => {
+    if (e.target.name === "sessionTime") {
+      if (this.state.sessionTime > 0) {
+        this.setState((prevState) => ({
+          sessionTime: --prevState.sessionTime,
+        }));
+      }
+    }
+    if (e.target.name === "breakeTime") {
+      if (this.state.breakTime > 0) {
+        this.setState((prevState) => ({
+          breakTime: --prevState.breakTime,
+        }));
+      }
+    }
+  };
+  timeAddition = (e) => {
+    if (e.target.name === "sessionTime") {
+      this.setState((prevState) => ({
+        sessionTime: ++prevState.sessionTime,
+      }));
+    }
+    if (e.target.name === "breakeTime") {
+      this.setState((prevState) => ({
+        breakTime: ++prevState.breakTime,
+      }));
+    }
+  };
+
+  timeSet = () => {
+    const sessionTime = this.state.sessionTime;
+    const breakeTime = this.state.breakTime;
+    this.props.onTimeChage(sessionTime, breakeTime);
+    this.setState({
+      settingsIsActive: !this.state.settingsIsActive,
+    });
+  };
   render() {
     return (
       <>
@@ -22,20 +73,54 @@ class Settings extends Component {
           <label className="timer-settings_label" htmlFor="session">
             Session timer
           </label>
-          <button className="timer-settings_button">-</button>
+          <button
+            onClick={this.timeSubtraction}
+            className="timer-settings_button"
+            name="sessionTime"
+          >
+            -
+          </button>
           <input
             className="timer-settings-input"
             type="number"
             id={"session"}
+            value={this.state.sessionTime}
+            readOnly
           />
-          <button className="timer-settings_button">+</button>
+          <button
+            onClick={this.timeAddition}
+            className="timer-settings_button"
+            name="sessionTime"
+          >
+            +
+          </button>
           <label htmlFor="breake" className="timer-settings_label">
             Breake timer
           </label>
-          <button className="timer-settings_button">-</button>
-          <input className="timer-settings-input" type="number" id={"breake"} />
-          <button className="timer-settings_button">+</button>
-          <button className="timer-settings_button-set">Set</button>
+          <button
+            onClick={this.timeSubtraction}
+            className="timer-settings_button"
+            name="breakeTime"
+          >
+            -
+          </button>
+          <input
+            className="timer-settings-input"
+            type="number"
+            id={"breake"}
+            value={this.state.breakTime}
+            readOnly
+          />
+          <button
+            onClick={this.timeAddition}
+            className="timer-settings_button"
+            name="breakeTime"
+          >
+            +
+          </button>
+          <button onClick={this.timeSet} className="timer-settings_button-set">
+            Set
+          </button>
         </div>
         <button
           onClick={this.handleShowHideSettings}
--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -3,14 +3,45 @@ import React, { Component } from "react";
 import Settings from "../components/Settings";
 import "../styles/Timer.css";
 class Timer extends Component {
-  state = {};
+  sessionTimeLocal;
+  breakTimeLocal;
+  state = { sessionTime: 25, breakTime: 5 };
+
+  componentDidMount() {
+    this.sessionTimeLocal = localStorage.getItem("sessionTime");
+    this.breakTimeLocal = localStorage.getItem("breakTime");
+    if (this.sessionTimeLocal) {
+      this.setState({ sessionTime: this.sessionTimeLocal });
+    } else {
+      localStorage.setItem("sessionTime", `${this.state.sessionTime}`);
+    }
+    if (this.breakTimeLocal) {
+      this.setState({ breakTime: this.breakTimeLocal });
+    } else {
+      localStorage.setItem("breakTime", `${this.state.breakTime}`);
+    }
+  }
+  componentDidUpdate() {
+    this.sessionTimeLocal = localStorage.setItem(
+      "sessionTime",
+      this.state.sessionTime
+    );
+    this.breakTimeLocal = localStorage.setItem(
+      "breakTime",
+      this.state.breakTime
+    );
+  }
+
+  timeUpdate = (sessionTime, breakTime) => {
+    this.setState({ sessionTime, breakTime });
+  };
 
   render() {
     return (
       <>
-        <Settings settingsIsActive={this.settingsIsActive} />
+        <Settings onTimeChage={this.timeUpdate} />
         <div className="timer">
-          <div className="timer_display">Time</div>
+          <div className="timer_display">{this.state.sessionTime}</div>
           <button className="timer_button start">Start</button>
           <button className="timer_button stop">Stop</button>
         </div>
