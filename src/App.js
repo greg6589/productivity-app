53ca81d6fd6c6b3b82f8f6e0bc1f7cc44f2a2823
Grzegorz <g.juzba@gmail.com>
2022-11-11 16:47:29 +0100

    move App.js to src folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import Navigation from "../layouts/Navigation";
-import Page from "../layouts/Page";
-import User from "../components/User/User";
-import UserContext from "../components/Context/SettingsContext";
+import Navigation from "./layouts/Navigation";
+import Page from "./layouts/Pages";
+import User from "./components/User/User";
+import UserContext from "./components/Context/SettingsContext";
 
 import style from "./App.module.css";
 
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <div className={style.App}>
       <Router>
-        <nav>{<Navigation />}</nav>
+        <Navigation />
         <UserContext.Provider
           value={{
             setUserLogged,
