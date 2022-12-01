8133f3be6eb81e66bb93dba490230c5117f719ef
Grzegorz <g.juzba@gmail.com>
2022-12-01 19:11:19 +0100

    put stop/play svg to one component

--- src/pages/Timer/Counter.js
+++ src/pages/Timer/Counter.js
@@ -8,6 +8,8 @@ import Button from "../../components/Button";
 import "react-circular-progressbar/dist/styles.css";
 import styles from "./Counter.module.css";
 
+import { StopSvg, PlaySvg } from "../../components/StopPlaySvg";
+
 const Counter = (props) => {
   const { sessionTime, breakTime } = props.value;
   const [isSessionActive, setIsSessionActive] = useState(false);
@@ -93,35 +95,7 @@ const Counter = (props) => {
       <Button
         handleClick={toggleCounter}
         className={styles.timer_button}
-        content={
-          isSessionActive ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V8a1 1 0 00-1-1H8z"
-                clipRule="evenodd"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                clipRule="evenodd"
-              />
-            </svg>
-          )
-        }
+        content={isSessionActive ? <StopSvg /> : <PlaySvg />}
       />
     </div>
   );
